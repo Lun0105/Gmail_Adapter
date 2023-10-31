1a2beb2d0e015b962f555685d98e55bab6a8184a
Lun0105 <76175876+Lun0105@users.noreply.github.com>
2023-10-31 16:11:06 +0800

    gmailAPI.py

--- gmailAPI.py
+++ gmailAPI.py
@@ -3,15 +3,21 @@ import email.header
 import os
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 class Mail:
     @staticmethod
     def decode_raw_mes(raw_data:str)->str:
+        if raw_data == None:
+            return "None"
         message = email.message_from_bytes(base64.urlsafe_b64decode(raw_data))
         return message
 
     @staticmethod
     def decode_mes_ele(mes:str)->str:
+        if mes == None:
+            return "None"
         mes = email.header.make_header(email.header.decode_header(mes))
         decoded_raw = str(mes)
         return decoded_raw  
@@ -52,45 +58,118 @@ class Mail:
                             f.write(part.get_payload(decode=True))
                         print(f"dowlaoding_file: {decoded_filename}")
         return 
+    
+    def decode_context(self)->str:
+        return None
 
 class Gmail:
     def __init__(self, email_address:str, token='token.json')->None:
         SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
         self.__email_address = email_address
-        self.__token = token
-        self.__creds = Credentials.from_authorized_user_file(self.__token, SCOPES)
+        self.__creds = Credentials.from_authorized_user_file(token, SCOPES)#None
+        if os.path.exists('token.json'):
+            self.__creds = Credentials.from_authorized_user_file(token, SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.__creds or not self.__creds.valid:
+            if self.__creds and self.__creds.expired and self.__creds.refresh_token:
+                self.__creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                self.__creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(self.__creds.to_json())
         self.__service = build('gmail', 'v1', credentials=self.__creds)
-        results = self.__service.users().messages().list(userId=self.__email_address).execute()
+        results = self.__service.users().messages().list(userId=self.__email_address, maxResults=50).execute()
         self.mailbox = results['messages'] #{'id':*, 'labelIds':*}
         return
     
+    def extendMailbox(self, num=50)->None:
+        results = self.__service.users().messages().list(userId=self.__email_address, maxResults=50+num).execute()
+        self.mailbox = results['messages'] #{'id':*, 'labelIds':*}
+        return
+
     def Print(self, amount=10)->None:
         i = 0
         for mail in self.mailbox:
-            spc = self.getById(mail['id'])
+            spc = self.FindById(mail['id'])
             spc.details()
             print()
             i+=1
-            if (i == amount):break;
+            if i == amount:break;
         return
     
-    def getById(self, ID:str)->Mail:
+    def FindById(self, ID:str)->Mail:
         res = self.__service.users().messages().get(userId=self.__email_address, id = ID, format='raw').execute()
         mail = Mail(res['raw'])
         return mail
     
-    def FindByTittle(self, tittle:str, Range=10)->Mail:
-        try:    
-            count = 0
+    def FindByTittle(self, tittle:str, limit=10)->Mail:
+        try:   
+            turn = 0
             for mail in self.mailbox:
-                count += 1
+                turn += 1
                 ID = mail['id']
                 res = self.__service.users().messages().get(userId=self.__email_address, id = ID, format='raw').execute()
                 this_mail = Mail(res['raw'])
                 if this_mail.head == tittle:
                     break
-                if count >= 10:
-                    assert False, f"mail named [{tittle}] not found in {Range} recent mails"
+                if turn >= limit:
+                    assert False, f"mail named [ {tittle} ] not found in {limit} recent mails"
             return this_mail    
         except AssertionError as msg:
             print(msg)
+
+    def FindByKeyword(self, keyword:str, consistance=True, limit=10)->list:
+        try:    
+            turn = 0
+            find = False
+            result =[]
+            for mail in self.mailbox:
+                turn += 1
+                ID = mail['id']
+                res = self.__service.users().messages().get(userId=self.__email_address, id = ID, format='raw').execute()
+                this_mail = Mail(res['raw'])
+                if consistance:
+                    if keyword in this_mail.head:
+                        find=True
+                        result.append(this_mail)
+                else:
+                    for keys in this_mail.head:
+                        if keys in keyword:
+                            find=True
+                            result.append(this_mail)
+                            break
+                if turn >= limit:
+                    if find:break;
+                    assert False, f"header includes [ {keyword} ] not found in {limit} recent mails"
+            return result
+        except AssertionError as msg:
+            print(msg)
+    
+    def FindBySender(self, sender:str, limit=10)->list:
+        try:    
+            turn = 0
+            result = []
+            find = False
+            for mail in self.mailbox:
+                turn += 1
+                ID = mail['id']
+                res = self.__service.users().messages().get(userId=self.__email_address, id = ID, format='raw').execute()
+                this_mail = Mail(res['raw'])
+                if sender in this_mail.sender:
+                    find=True
+                    result.append(this_mail)
+                if turn >= limit:
+                    if find:break;
+                    assert False, f"mail sended by {sender} not found in {limit} recent mails";
+            return result    
+        except AssertionError as msg:
+            print(msg)
+    
+    def sortLabel(self, label:str, idList:list)->None:
+        return
+
+    def sent_mes(self, target:str)->None:
+        return
